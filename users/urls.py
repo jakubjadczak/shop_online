3a809897319d4a9c4d8dfb8055a00d8bf25c97f7
Jakub Jadczak <jakubjadczak02@outlook.com>
2022-01-19 14:47:07 +0100

    Logowanie i rejestracja bez weryfikacji mailem

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import register_page, activate
+from .views import register_page, activate, user_login, user_logout
 
 app_name = 'users'
 
 
 urlpatterns = [
     path('register/', register_page, name='register'),
-    path('activate/<uidb64>/<token>', activate, name='activate')
+    path('activate/<uidb64>/<token>', activate, name='activate'),
+    path('login/', user_login, name='login'),
+    path('logout', user_logout, name='logout'),
 ]
