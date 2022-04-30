31c487e9f267c0861adc3bdc213157c4e6d44ccb
Jakub Jadczak <jakubjadczak02@outlook.com>
2022-04-30 19:51:27 +0200

    Delete and edit offers

--- offers/views.py
+++ offers/views.py
@@ -1,9 +1,10 @@
 from django.forms import modelformset_factory
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views import View
 from .forms import OfferForms, PhotoForms
 from .models import Photo, Offer
 from django.core.paginator import Paginator
+from django.contrib import messages
 
 
 class AddOffer(View):
@@ -50,6 +51,7 @@ class AddPhotos(View):
                     instance.save()
                     print('ok')
             del request.session['offer']
+            return redirect(reverse('main:home'))
         return render(
             request=request,
             template_name='offers/add_photos_to_offer.html'
@@ -155,3 +157,58 @@ def offer_detail(request, result_id):
         template_name='offers/offer_detail.html',
         context=context
     )
+
+
+class MyOffers(View):
+
+    @staticmethod
+    def post(request, *args, **kwargs):
+
+        return render(
+            request=request,
+            template_name='offers/my_offers.html'
+        )
+
+    @staticmethod
+    def get(request, *args, **kwargs):
+        result = Offer.objects.filter(owner_id=request.user)
+
+        paginator_result = Paginator(result, 2)
+        page_number = request.GET.get('page')
+        page_obj = paginator_result.get_page(page_number)
+
+        context = {
+            'result_list': page_obj,
+        }
+
+        return render(
+            request=request,
+            template_name='offers/my_offers.html',
+            context=context
+        )
+
+
+def delete_offer(request, result_id):
+    offer = Offer.objects.filter(pk=result_id)
+    offer.delete()
+    messages.add_message(request, messages.SUCCESS, 'Usunięto oferte')
+    return redirect(reverse('offer:my_offer'))
+
+
+def edit_offer(request, result_id):
+    offer = get_object_or_404(Offer, pk=result_id)
+    if request.method == 'POST':
+        form = OfferForms(request.POST, request.FILES, instance=offer)
+        if form.is_valid():
+            form.save()
+            request.session['offer'] = result_id
+            return redirect(reverse('offer:add_photos'))
+    else:
+        form = OfferForms(instance=offer)
+
+    return render(
+        request=request,
+        template_name='offers/add_offer.html',
+        context={'form': form}
+    )
+
